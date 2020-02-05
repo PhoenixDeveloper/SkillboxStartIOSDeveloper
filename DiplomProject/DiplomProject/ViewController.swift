d42d5bb6c7eceda1c3439e273730bd399b1681c4
Mikhail <mefisto40000x@gmail.com>
2020-02-05 11:17:21 +0300

    Создан проект. Импортированы Pod-библиотеки

---
+++ DiplomProject/DiplomProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiplomProject
+//
+//  Created by Михаил Беленко on 05.02.2020.
+//  Copyright © 2020 Михаил Беленко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
