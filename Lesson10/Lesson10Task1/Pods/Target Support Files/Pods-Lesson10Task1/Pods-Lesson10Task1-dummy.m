287c5f3a31876016ad846d320ac30eef103641d9
Mikhail Belenko <mefisto40000x@gmail.com>
2020-09-25 11:58:10 +0300

    Удалены лишние проекты из ветки

--- Lesson10/Lesson10Task1/Pods/Target Support Files/Pods-Lesson10Task1/Pods-Lesson10Task1-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Lesson10Task1 : NSObject
-@end
-@implementation PodsDummy_Pods_Lesson10Task1
-@end
--- Lesson12/Pods/Target Support Files/Pods-Lesson12/Pods-Lesson12-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Lesson12 : NSObject
-@end
-@implementation PodsDummy_Pods_Lesson12
-@end
